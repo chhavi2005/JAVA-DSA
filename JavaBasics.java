342b9c932821ba339c449863db321589e823aa0b
chhavi2005 <124762731+chhavi2005@users.noreply.github.com>
2024-02-01 09:16:01 +0530

    The DSA Journey Starts Here--------
    In this program i have learnt the basic syntax of JAVA , and that the name of the class should be same as the name of the file,and also how to print in Java by different types (through print, println, \n).........

---
+++ JavaBasics.java
@@ -0,0 +1,10 @@
+//boilerplate code
+public class JavaBasics {           //class 
+    public static void main(String[] args) {              //function
+        System.out.println("Hello Chhavi");
+        System.out.println("Hello Chhavi");           //output through ln
+
+        System.out.print("Hello Chhavi\n");           //output through print with/n
+        System.out.print("Hello \nChhavi");
+    }
+}

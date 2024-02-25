580c404182438e5efc9d11360bd5def91e5da6cc
Chhavi Gajnani <124762731+chhavi2005@users.noreply.github.com>
2024-02-25 22:13:40 +0530

    Number Pyramid in Java
    In this program i learnt how to print number pyramid pattern in Java.

---
+++ numberPyramid.java
@@ -0,0 +1,19 @@
+public class numberPyramid {
+    public static void pyramid(int n){
+        //lines
+        for(int i = 1;i<=n;i++){
+            //spaces
+            for(int j =1 ;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            //numbers
+            for(int j =1;j<=i;j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        pyramid(5);
+    }
+}

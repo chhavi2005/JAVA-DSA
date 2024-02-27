9f1dfa9103957202be2356de4cf93975d02dfeee
chhavi2005 <chhavigajnani5@gmail.com>
2024-02-27 10:46:58 +0530

    DSA with Java

---
+++ Arrays/binarySearch.java
@@ -0,0 +1,7 @@
+public class binarySearch {
+    //binary search is used for sorted arrays (example like dictionary works)
+    public static void main(String[] args) {
+        int number[]={2,4,6,8,10,12,14};
+        int key =10;
+    }
+}
---
+++ Pattern/palindromeWithNumbers.java
@@ -0,0 +1,8 @@
+public class palindromeWithNumbers {
+    public static void palindrome(int n){
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
